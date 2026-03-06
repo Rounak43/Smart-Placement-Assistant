331fab6396cbc5cd1d6717be05b3deca9962e5e6
Rounak43 <rounaks135@gmail.com>
2026-03-06 17:00:50 +0530

    updated some errors

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/navbar'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 import '../styles/Dashboard.css';
 
 export default function Dashboard({ user }) {
