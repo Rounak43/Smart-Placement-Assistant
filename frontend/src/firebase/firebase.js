5367c2cdc9d4ead0b77c196afcc81f71456f5aab
rounak sharma <167230272+Rounak43@users.noreply.github.com>
2026-04-07 21:04:01 +0530

    changed api key
    Updated Firebase configuration to use environment variables for sensitive information.

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBbokqbds74PKhKSHAdBNNgEKerffi8cAU",
-  authDomain: "smat-placement-platform.firebaseapp.com",
-  projectId: "smat-placement-platform",
-  storageBucket: "smat-placement-platform.firebasestorage.app",
-  messagingSenderId: "868390484596",
-  appId: "1:868390484596:web:4124012ef9e3ca58542f21",
-  measurementId: "G-1F7BGYN90W"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderID: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
